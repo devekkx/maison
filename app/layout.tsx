d1fcb02f34b9ae7f108913b37ad3cfde27e32b6c
Emmanuel Kpendo <devekkx@gmail.com>
2026-07-03 10:50:00 +0000

    feat: add skip-to-main-content link in layout for keyboard navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,15 @@ export default function RootLayout({
       lang="en"
       className={`${cormorant.variable} ${instrumentSerif.variable} ${jakartaSans.variable} ${jetbrainsMono.variable}`}
     >
-      <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>
+        <a
+          href="#main"
+          className="skip-link sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[9999] focus:px-4 focus:py-2 focus:bg-white focus:text-black focus:rounded"
+        >
+          Skip to main content
+        </a>
+        {children}
+      </body>
     </html>
   );
 }
